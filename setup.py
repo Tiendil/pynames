a599e3ba1b72702f467c04b886c0584ebc3d5d27
Tiendil <a.eletsky@gmail.com>
2012-01-18 23:43:37 +0300

    prepair package

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import setuptools
+
+setuptools.setup(
+    name = 'Pynames',
+    version = '0.1.0',
+    author = 'Aleksey Yeletsky',
+    author_email = 'a.eletsky@gmail.com',
+    packages = setuptools.find_packages(),
+    url = 'https://github.com/Tiendil/pynames',
+    license = 'LICENSE',
+    description = "characters' name generation library",
+    long_description = open('README.md').read(),
+    include_package_data = True, # setuptools-git MUST be installed
+    test_suite = 'tests'
+)
